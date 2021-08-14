5f3df4f938b6efef2e66d02842384f796e001511
Mehul Mohan <mehulmpt@gmail.com>
2021-08-14 19:55:21 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function App() {
-	return <div className="App"></div>
+	const counter = 0
+
+	return (
+		<div className="App">
+			<h1>Counter: {counter}</h1>
+			<button>Increment</button>
+			<button>Decrement</button>
+		</div>
+	)
 }
 
 export default App
