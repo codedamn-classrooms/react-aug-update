3aebd69ef2b6cdf88cce0e4fd034e6fb3113d33e
Mehul Mohan <mehulmpt@gmail.com>
2021-08-14 20:29:37 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+	const [visible, setVisible] = useState(true)
+
 	return (
 		<div className="App">
-			{/* give this input correct ID and "enter" event */}
-			<input type="text" />
+			{/* Render this component conditionally */}
+			<div id="contents">
+				codedamn helps me to learn interactively by building these apps
+				inside browser. Fun, isn't it?
+			</div>
 
-			<ul id="task-list">
-				{/* your tasks should be added here*/}
-			</ul>
+			{/* the label of this button should be dynamic */}
+			<button>hide</button>
 		</div>
 	)
 }
