9a0c8cfd29fe4387235c72a281b8061ed013617f
Pranav <pranav.techiegeek@gmail.com>
2023-11-07 14:26:10 +0530

    feat: shift to bun

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-	import.meta.hot.accept()
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
