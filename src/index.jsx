b0853de728c2e0b941534e5607c691430df2071a
Pranav <pranav.techiegeek@gmail.com>
2023-11-07 13:43:45 +0530

    remove snowpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
 );
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-	import.meta.hot.accept();
-}
